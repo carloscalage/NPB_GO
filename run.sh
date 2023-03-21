bbe8a2ea93c64c7566751a75e4a271500dc68016
Ariamb <ariambartsch@gmail.com>
2023-03-21 14:43:31 -0300

    detailed time

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ N=20
 sum1=0
 
 
-printf "Running serial EP\n"
+printf "Running parallel EP\n"
 for i in $(seq 1 $N); do
     start=$(date +%s%N) #nanosegundos
     go run ep.go
@@ -17,6 +17,7 @@ done
 
 avg=$(echo "$sum1 / $N" | bc -l)
 ml=$(echo "$sum1 / 1000000" | bc -l)
+avgsec=$(echo "$ml / $N / 60" | bc -l)
 
 printf "time elapsed in nanoseconds: %0.0f, time elapsed in miliseconds: %0.1f\n" $avg $ml
-
+printf "average time in seconds: %0.1f\n" $avgsec
