694634aad131439aae6ae4f106e82524fec771c5
Carlos Calage <calage.carlos@gmail.com>
2023-05-06 17:44:39 -0300

    feat: running all classes on both languages intercalated

--- run.sh
+++ run.sh
@@ -2,50 +2,45 @@
 
 read -p "Qual kernel deseja executar? (EP, IS): " KERNEL
 
-read -p "Em qual linguagem deseja executar? (GO, C): " LANG
-
-#file_name='echo "print '${KERNEL}'.lower()" | python'
-#echo $file_name
-
-echo "Running $KERNEL - $CLASS in $LANG"
-
 N=30
 
 echo >> results/log.csv
 
-if [ $LANG = C ]; then
-    cd GMAP/NPB-OMP
-    make clean
-fi
+# Compiling files
 
-if [ $LANG = GO ]; then
-    cd IS
-fi
+# Building GO
+cd $KERNEL
+go build
 
-for CLASS in S W A B C D E; do
+# Building C
+cd ../GMAP/NPB-OMP
+make clean
+
+for CLASS in S W A B; do
+    make ${KERNEL} CLASS=${CLASS}
+done
+
+cd ../../
+
+# Running All The Classes 
+for CLASS in S W A B; do
     echo $CLASS
-    if [ $LANG = GO ]; then
-        for i in $(seq 1 $N); do
+
+    for i in $(seq 1 $N); do
         echo $i
-        go build
+
+	echo C
+        start=$(date +%s%N)
+        ./GMAP/NPB-OMP/bin/is.$CLASS
+        end=$(date +%s%N)
+        time=$((end-start))
+        echo "${KERNEL},${CLASS},C,${time}" >> ../results/log.csv
+
+        echo GO
         start=$(date +%s%N)
         ./${KERNEL} ${CLASS}
-        end=$(date +%s%N) 
+        end=$(date +%s%N)
         time=$((end-start))
-        echo "${KERNEL},${CLASS},serial,${LANG},${time}" >> ../results/log.csv
-        done
-    fi
-
-    if [ $LANG = C ]; then
-        make ${KERNEL} CLASS=${CLASS}
-        for i in $(seq 1 $N); do
-        echo $i
-        start=$(date +%s%N)
-        ./bin/is.${CLASS}
-        end=$(date +%s%N) 
-        time=$((end-start))
-        echo "${KERNEL},${CLASS},serial,${LANG},${time}" >> ../../results/log.csv
-        done
-    fi
-
+        echo "${KERNEL},${CLASS},GO,${time}" >> ../results/log.csv
+    done
 done
